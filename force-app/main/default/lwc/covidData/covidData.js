fa88b5d63a2499fe41f432cbe77360fec142e75a
Akashreddy19 <girijalac@gmail.com>
2024-09-05 23:07:45 +0530

    more changes to covidData

--- force-app/main/default/lwc/covidData/covidData.js
+++ force-app/main/default/lwc/covidData/covidData.js
@@ -1,24 +1,28 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 import covidRESTCall from "@salesforce/apex/CovidInfoREST.covidRESTCall";
 
 export default class CovidData extends LightningElement {
+  @track isError = false;
+  @track covidData;
   countryCode;
   onCountryCodeChange(event) {
     this.countryCode = event.target.value;
     console.log(event.target.value);
+    this.isError = false;
   }
   getCovidDetails() {
     console.log("Button clicked.. countryCode", this.countryCode);
-    covidRESTCall(this.countryCode)
-      .then((data, error) => {
-        if (data) {
-          console.log("Response From Server:", JSON.stringify(data));
-        } else if (error) {
-          console.log("Error: ", error);
+    covidRESTCall({ countryCode: this.countryCode })
+      .then((data) => {
+        console.log("Response From Server:", JSON.stringify(data));
+        this.covidData = data;
+        if (data.length === 0) {
+          this.isError = true;
         }
       })
       .catch((err) => {
         console.log(err);
+        this.isError = true;
       });
   }
 }
